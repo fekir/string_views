9059368dccd811464dacedf376b93001f6413457
Federico Kircheis <federico.kircheis@gmail.com>
2020-11-18 21:35:47 +0100

    Split implementation
    Move check to another class

--- include/string_views/string_views.hpp
+++ include/string_views/string_views.hpp
@@ -8,11 +8,55 @@
 
 namespace string_views {
 
+// everything explicit, minimal interface
+// plus empty, as convenience
+template<typename character>  //
+struct buff_view {
+	constexpr buff_view(const character* data_, std::size_t size_) noexcept : m_data{data_}, m_size{size_} {
+	}
+	constexpr const character* data() const noexcept {
+		return m_data;
+	}
+	constexpr std::size_t size() const noexcept {
+		return m_size;
+	}
+	constexpr bool empty() const noexcept {
+		return this->size() == 0;
+	}
+
+private:
+	const character* m_data;
+	std::size_t m_size;
+};
+
+template<class character> struct default_content_policy {
+	static constexpr void check(const character* begin, const character* end) noexcept {
+		(void) begin;
+		(void) end;
+	}
+};
+
+template<typename character, class content_policy>  //
+struct checked_buff_view : private buff_view<character> {
+	constexpr checked_buff_view(const character* begin_, std::size_t size_) : buff_view<character>(begin_, size_) {
+		content_policy::check(this->data(), this->data() + this->size());
+	}
+	using buff_view<character>::data;
+	using buff_view<character>::size;
+	using buff_view<character>::empty;
+};
+
+namespace detail {
+template<typename character> unsigned long constexpr len(const character* str) {
+	return *str ? 1u + len(++str) : 0u;
+}
+}  // namespace detail
 // should this apply to both const char* and std::string? what about operator const char*?
 enum class conversion_policy {
 	implicit_,
 	explicit_,
 };  // FIXME: find better name as explicit reserved
+
 enum class format_policy {
 	zero_terminated,
 	not_zero_terminated,
@@ -26,14 +70,10 @@ enum debug_policy {
 #else
 	global = unchecked,
 #endif
-}; // FIXME: implementation is incomplete
+};  // FIXME: implementation is incomplete
 
 // constexpr strlen
 namespace detail {
-template<class T> long constexpr len(const T* str) {
-	return *str ? 1 + len(++str) : 0;
-}
-
 template<enum debug_policy p> using iterator_type = std::integral_constant<debug_policy, p>;
 using unchecked_debug = iterator_type<debug_policy::unchecked>;
 using checked_debug = iterator_type<debug_policy::checked>;
@@ -57,20 +97,13 @@ template<class I> constexpr I make_iterator(unchecked_debug, I it, std::size_t, 
 
 }  // namespace detail
 
-template<class character> struct default_content_policy {
-	static constexpr void check(const character* begin, const character* end) noexcept {
-		(void) begin;
-		(void) end;
-	}
-};
-
 template<class character = char,                                                        //
      conversion_policy explicit_constructor_from_charp = conversion_policy::explicit_,  //
      format_policy format = format_policy::zero_terminated,                             //
      class content_policy = default_content_policy<character>,                          //
      debug_policy debug = debug_policy::global                                          //  does not change constructors behaviour
      >
-struct basic_string_views {
+struct basic_string_views : private checked_buff_view<character, content_policy> {
 	using difference_type = ptrdiff_t;
 	using size_type = std::size_t;  // TODO: consider signed...
 	using value_type = character;
@@ -82,104 +115,101 @@ struct basic_string_views {
 	/// constructor from char*
 	/// for all policies, and conditional explicit
 	template<typename _Dummy = void,
-		 typename std::enable_if<(explicit_constructor_from_charp == conversion_policy::explicit_)
-				   //&& (format == format_policy::zero_terminated)  //
-				   && std::is_void<_Dummy>::value,
-			  character>::type = false>
-	constexpr explicit basic_string_views(const value_type* data) : m_begin{data}, m_end{data + detail::len(data)} {
-		content_policy::check(m_begin, m_end);
+	     typename std::enable_if<(explicit_constructor_from_charp == conversion_policy::explicit_)
+	               //&& (format == format_policy::zero_terminated)  //
+	               && std::is_void<_Dummy>::value,
+	          character>::type = false>
+	constexpr explicit basic_string_views(const value_type* data_) :
+	            checked_buff_view<character, content_policy>(data_, detail::len(data_)) {
 	}
 
 	constexpr basic_string_views(std::nullptr_t) = delete;
 
 	template<typename _Dummy = void,
-		 typename std::enable_if<(explicit_constructor_from_charp == conversion_policy::implicit_)
-				   //&& (format == format_policy::zero_terminated)  //
-				   && std::is_void<_Dummy>::value,
-			  character>::type = true>
-	constexpr basic_string_views(const value_type* data) : m_begin{data}, m_end{data + detail::len(data)} {
-		content_policy::check(m_begin, m_end);
+	     typename std::enable_if<(explicit_constructor_from_charp == conversion_policy::implicit_)
+	               //&& (format == format_policy::zero_terminated)  //
+	               && std::is_void<_Dummy>::value,
+	          character>::type = true>
+	constexpr basic_string_views(const value_type* data_) : checked_buff_view<character, content_policy>(data_, detail::len(data_)) {
 	}
 
 	/// constructor from iterators
 	/// for all format_policy, conditional explicit
-	constexpr explicit basic_string_views(const value_type* begin, const value_type* end) : m_begin{begin}, m_end{end} {
-		content_policy::check(m_begin, m_end);
+	constexpr basic_string_views(const value_type* begin, const value_type* end) :
+	            checked_buff_view<character, content_policy>(begin, end - begin) {
 	}
-	constexpr explicit basic_string_views(const value_type* begin, size_type size) : m_begin{begin}, m_end{begin + size} {
-		content_policy::check(m_begin, m_end);
+	constexpr basic_string_views(const value_type* begin, size_type size_) :
+	            checked_buff_view<character, content_policy>(begin, size_) {
 	}
 
 	/// constructor from self
 	/// for all format_policy, conditional explicit
 	template<typename _Dummy = void,  //
-		 conversion_policy p,         //
-		 format_policy f,             //
-		 debug_policy d,              //
-		 typename std::enable_if<(explicit_constructor_from_charp == conversion_policy::explicit_) && std::is_void<_Dummy>::value,
-			  character>::type = true>
+	     conversion_policy p,         //
+	     format_policy f,             //
+	     debug_policy d,              //
+	     typename std::enable_if<(explicit_constructor_from_charp == conversion_policy::explicit_) && std::is_void<_Dummy>::value,
+	          character>::type = true>
 	constexpr explicit basic_string_views(basic_string_views<character, p, f, content_policy, d> sv) :
-				m_begin{sv.begin()}, m_end{sv.end()} {
+	            checked_buff_view<character, content_policy>(sv.data(), sv.size()) {
 		static_assert(f == format_policy::zero_terminated || f == format, "not compatible format policy");
-		content_policy::check(m_begin, m_end);
 	}
 	template<typename _Dummy = void,  //
-		 conversion_policy p,         //
-		 format_policy f,             //
-		 debug_policy d,              //
-		 typename std::enable_if<(explicit_constructor_from_charp == conversion_policy::implicit_) && std::is_void<_Dummy>::value,
-			  character>::type = true>
-	constexpr basic_string_views(basic_string_views<character, p, f, content_policy, d> sv) : m_begin{sv.begin()}, m_end{sv.end()} {
+	     conversion_policy p,         //
+	     format_policy f,             //
+	     debug_policy d,              //
+	     typename std::enable_if<(explicit_constructor_from_charp == conversion_policy::implicit_) && std::is_void<_Dummy>::value,
+	          character>::type = true>
+	constexpr basic_string_views(basic_string_views<character, p, f, content_policy, d> sv) :
+	            checked_buff_view<character, content_policy>(sv.data(), sv.size()) {
 		static_assert(f == format_policy::zero_terminated || f == format, "not compatible format policy");
-		content_policy::check(m_begin, m_end);
 	}
 
 	/// constructor from std string object - does not work for string_view
 	/// for all format_policy, conditional explicit
 	template<class string,
-		 typename std::enable_if<explicit_constructor_from_charp == conversion_policy::explicit_ && !std::is_void<string>::value,
-			  character>::type = true>
-	constexpr explicit basic_string_views(const string& s) : m_begin{s.c_str()}, m_end{s.c_str() + s.size()} {
-		content_policy::check(m_begin, m_end);
+	     typename std::enable_if<explicit_constructor_from_charp == conversion_policy::explicit_ && !std::is_void<string>::value,
+	          character>::type = true>
+	constexpr explicit basic_string_views(const string& s) : checked_buff_view<character, content_policy>(s.c_str(), s.size()) {
 	}
 	template<class string,
-		 typename std::enable_if<explicit_constructor_from_charp == conversion_policy::implicit_ && !std::is_void<string>::value,
-			  character>::type = true>
-	constexpr basic_string_views(const string& s) : m_begin{s.c_str()}, m_end{s.c_str() + s.size()} {
-		content_policy::check(m_begin, m_end);
+	     typename std::enable_if<explicit_constructor_from_charp == conversion_policy::implicit_ && !std::is_void<string>::value,
+	          character>::type = true>
+	constexpr basic_string_views(const string& s) : checked_buff_view<character, content_policy>(s.c_str(), s.size()) {
 	}
 
-	/// accessor functions
-	constexpr const_pointer data() const noexcept {
-		return this->m_begin;
-	}
+	/// minimal interface
+	using buff_view<character>::data;
+	using buff_view<character>::size;
+	using buff_view<character>::empty;
+
 	// only for policy zero-term
 	template<typename _Dummy = void,  //
-		 typename std::enable_if<(format == format_policy::zero_terminated) && std::is_void<_Dummy>::value, character>::type = true>
+	     typename std::enable_if<(format == format_policy::zero_terminated) && std::is_void<_Dummy>::value, character>::type = true>
 	constexpr const_pointer c_str() const noexcept {
-		return this->m_begin;
+		return this->data();
 	}
 	constexpr const_reference front() const {
 		assert(debug == debug_policy::unchecked || !(this->empty() && "out of bound access"));
-		return *(this->m_begin);
+		return *(this->data());
 	}
 	constexpr const_reference back() const {
 		assert(debug == debug_policy::unchecked || !(this->empty() && "out of bound access"));
-		return *(this->m_end - 1);
+		return *(this->data() + this->size() - 1);
 	}
 
 	/// iterators
 	constexpr auto begin() const noexcept {
-		return detail::make_iterator(detail::iterator_type<debug>{}, this->m_begin, this->size());
+		return detail::make_iterator(detail::iterator_type<debug>{}, this->data(), this->size());
 	}
 	constexpr auto end() const noexcept {
-		return detail::make_iterator(detail::iterator_type<debug>{}, this->m_begin, this->size(), this->size());
+		return detail::make_iterator(detail::iterator_type<debug>{}, this->data(), this->size(), this->size());
 	}
 	constexpr auto cbegin() const noexcept {
-		return detail::make_iterator(detail::iterator_type<debug>{}, this->m_begin, this->size());
+		return detail::make_iterator(detail::iterator_type<debug>{}, this->data(), this->size());
 	}
 	constexpr auto cend() const noexcept {
-		return detail::make_iterator(detail::iterator_type<debug>{}, this->m_begin, this->size(), this->size());
+		return detail::make_iterator(detail::iterator_type<debug>{}, this->data(), this->size(), this->size());
 	}
 	// safe/implicit only for 0-terminated, do we need it?
 	/*	explicit operator const_pointer() {
@@ -188,8 +218,8 @@ struct basic_string_views {
 
 	// FIXME: add overloads for integral types, and disallow enum, bool, characters, ...
 	constexpr const_reference operator[](size_type s) const {
-		assert(debug == debug_policy::unchecked || (s >= 0 && s < this->size() && "out of bound access"));
-		return this->m_begin[s];
+		assert(debug == debug_policy::unchecked || (s < this->size() && "out of bound access"));
+		return this->data()[s];
 	}
 	// reverse iterators
 
@@ -200,23 +230,12 @@ struct basic_string_views {
 	// non_empty_string_view should make a substring that is not empty...
 	// otherwise make this method generic, and throught template param make
 	basic_string_views substring() {
-		return basic_string_views(this->m_begin, this->m_end);
-	}
-
-	constexpr size_type size() const noexcept {
-		return static_cast<size_type>(this->m_end - this->m_begin);
-	}
-	constexpr bool empty() const noexcept {
-		return this->m_end == this->m_begin;
+		return basic_string_views(this->data(), this->data() + this->size());
 	}
 
 	// for string_view pop_back works on non-empty
 	// for zstring_view pop_back never works
 	// for non_empty_string_view pop_back works on size >=2
-
-private:
-	const_pointer m_begin;
-	const_pointer m_end; // TODO: consider storing length
 };
 
 }  // namespace string_views
